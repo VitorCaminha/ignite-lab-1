37f53bbb9c3432d572f13e56fe1ceddf83500ab2
Vitor Leao Caminha <vitorcaminha@or.com.br>
2022-08-01 16:16:51 -0300

    feat: integrate graphql with codegen to make requests via client and server side

--- web/src/pages/app/index.tsx
+++ web/src/pages/app/index.tsx
@@ -1,21 +1,31 @@
-import { getAccessToken, withPageAuthRequired } from "@auth0/nextjs-auth0";
+import { withPageAuthRequired } from "@auth0/nextjs-auth0";
 
-export default function Home() {
+import { withApollo } from "../../lib/withApollo";
+
+import { useMeQuery } from "../../graphql/generated/graphql";
+import { getServerPageGetProducts, ssrGetProducts } from "../../graphql/generated/page";
+
+function Home({ data }) {
+  const { data: me } = useMeQuery();
+
   return (
-    <div>
+    <div className="text-violet-500">
       <h1>Hello World</h1>
 
+      <pre>{JSON.stringify(data.products, null, 2)}</pre>
+      <pre>{JSON.stringify(me, null, 2)}</pre>
+      
       <a href="/api/auth/logout">Logout</a>
     </div>
   )
 }
 
 export const getServerSideProps= withPageAuthRequired({
-  getServerSideProps: async ({ req, res}) => {
-    console.log(getAccessToken(req, res))
-
-    return {
-      props: {}
-    }
+  getServerSideProps: async (ctx) => {
+    return getServerPageGetProducts({}, ctx);
   }
 });
+
+export default withApollo(
+  ssrGetProducts.withPage()(Home)
+);
